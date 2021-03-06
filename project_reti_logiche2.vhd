3e876198bca93de3373b51631b88bff5bf172b3a
hazem <shalbyhazem99@hotmail.com>
2021-03-06 12:45:11 +0100

    piccola modifica a shift_level

--- project_reti_logiche2.vhd
+++ project_reti_logiche2.vhd
@@ -1,8 +1,8 @@
 ----------------------------------------------------------------------------------
 -- PROGETTO DI RETI LOGICHE by PROF. GIANLUCA PALERMO
 --
--- SHALBY HAZEM HESHAM YOUSEF   (10596243)
--- PEREGO NICCOLO'              (10628782)
+-- SHALBY HAZEM HESHAM YOUSEF (10596243)
+-- PEREGO NICCOLO' (10628782)
 ----------------------------------------------------------------------------------
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
@@ -29,23 +29,20 @@ ARCHITECTURE Behavioral OF project_reti_logiche IS
 BEGIN
 	PROCESS (i_clk, i_rst)
 	VARIABLE temp_integer : INTEGER;
-	VARIABLE temp_data    : std_logic_vector(15 DOWNTO 0);
 	BEGIN
 		IF i_rst = '1' THEN
-			o_done     <= '0';
-			o_en       <= '0';
 			next_state <= WAIT_START;
 		ELSIF rising_edge(i_clk) THEN
 			CASE next_state IS
 				WHEN WAIT_START =>
-					o_done <= '0';
 					IF i_start = '1' THEN
 						max         <= (OTHERS => '0');
 						min         <= (OTHERS => '1');
 						count       <= 0;
-						shift_level <= 3; --va bene qualsiasi valore che non può essere assunto da shift_level
+						shift_level <= 9;
 						next_state  <= READ_REQ;
 					ELSE
+						o_done     <= '0';
 						next_state <= WAIT_START;
 					END IF;
 				WHEN READ_REQ =>
@@ -59,14 +56,15 @@ BEGIN
 					ELSIF count = 1 THEN
 						next_state <= READ_ROW;
 					ELSE
-						IF shift_level = 3 THEN
+						IF shift_level > 8 THEN
 							next_state <= COMPARE_DATA;
 						ELSE
 							next_state <= ELABORATE_DATA;
 						END IF;
 					END IF;
-					temp_integer := count + 1; --INCREMENTO DI COUNT
-					count <= temp_integer;
+					--INCREMENTO DI COUNT
+					temp_integer := count;
+					count <= temp_integer + 1;
 				WHEN READ_COLUMN =>
 					byte_to_read <= TO_INTEGER(unsigned (i_data));
 					next_state   <= READ_REQ;
@@ -86,6 +84,7 @@ BEGIN
 						IF i_data > max THEN
 							max <= i_data;
 						END IF;
+						--incremento count
 						next_state <= READ_REQ;
 					ELSE
 						count      <= 2;
@@ -96,23 +95,23 @@ BEGIN
 					--calcolo delta value e shif level
 					temp_integer := TO_INTEGER(unsigned (max)) - TO_INTEGER(unsigned (min)); --delta_value
 					IF temp_integer = 0 THEN
-						shift_level <= 256;
-					ELSIF temp_integer > 0 AND temp_integer < 3 THEN
-						shift_level <= 128;
-					ELSIF temp_integer > 2 AND temp_integer < 7 THEN
-						shift_level <= 64;
-					ELSIF temp_integer > 6 AND temp_integer < 15 THEN
-						shift_level <= 32;
-					ELSIF temp_integer > 14 AND temp_integer < 31 THEN
-						shift_level <= 16;
-					ELSIF temp_integer > 30 AND temp_integer < 63 THEN
 						shift_level <= 8;
-					ELSIF temp_integer > 62 AND temp_integer < 127 THEN
+					ELSIF temp_integer > 0 AND temp_integer < 3 THEN
+						shift_level <= 7;
+					ELSIF temp_integer > 2 AND temp_integer < 7 THEN
+						shift_level <= 6;
+					ELSIF temp_integer > 6 AND temp_integer < 15 THEN
+						shift_level <= 5;
+					ELSIF temp_integer > 14 AND temp_integer < 31 THEN
 						shift_level <= 4;
-					ELSIF temp_integer > 126 AND temp_integer < 255 THEN
+					ELSIF temp_integer > 30 AND temp_integer < 63 THEN
+						shift_level <= 3;
+					ELSIF temp_integer > 62 AND temp_integer < 127 THEN
 						shift_level <= 2;
-					ELSE
+					ELSIF temp_integer > 126 AND temp_integer < 255 THEN
 						shift_level <= 1;
+					ELSE
+						shift_level <= 0;
 					END IF;
 					next_state <= READ_REQ;
 				WHEN ELABORATE_DATA =>
@@ -122,8 +121,8 @@ BEGIN
 						o_we      <= '1';
 						o_address <= std_logic_vector(to_unsigned(count + byte_to_read - 1, 16)); --conversione su 16 bit
 						--calcolo new_value del pixel corrente da scrivere
-						temp_integer := to_integer(unsigned(i_data) - unsigned(min)) * shift_level;
-						IF temp_integer >= 255 THEN
+						temp_integer := (to_integer(unsigned(i_data) - unsigned(min))) * 2 ** shift_level;
+						IF temp_integer > 255 THEN
 							o_data <= (OTHERS => '1');
 						ELSE
 							o_data <= std_logic_vector(to_unsigned(temp_integer, 8));
